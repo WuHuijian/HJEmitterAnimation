4cd9efbdea51dc7f9cae7d45e84479838e7ffe5f
WHJ <760503653@qq.com>
2017-12-14 16:10:02 +0800

    Initial Commit

---
+++ HJEmitterAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HJEmitterAnimation
+//
+//  Created by WHJ on 2017/12/14.
+//  Copyright © 2017年 WHJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HJEmitterAnimation/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  HJEmitterAnimation
+//
+//  Created by WHJ on 2017/12/14.
+//  Copyright © 2017年 WHJ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
